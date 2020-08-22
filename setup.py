368447adf6a094c12291ea2da1b88dd32783db68
Ed Rogers <etfrogers@hotmail.com>
2020-08-22 19:51:02 +0100

    Fix flake8 errors in known_transforms.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyhank",
-    version="1.0.0",
+    version="1.1.0",
     author="Edward Rogers",
     author_email="etfrogers@hotmail.com",
     description="pyhank - Quasi-discrete Hankel transforms for python",
