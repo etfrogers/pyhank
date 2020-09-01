2604f4fcbc4bb33c2186410b19cf117aa1b368de
Ed Rogers <etfrogers@hotmail.com>
2020-09-01 18:04:57 +0100

    Set version to 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyhank",
-    version="2.0.0",
+    version="2.1.0",
     author="Edward Rogers",
     author_email="etfrogers@hotmail.com",
     description="pyhank - Quasi-discrete Hankel transforms for python",
